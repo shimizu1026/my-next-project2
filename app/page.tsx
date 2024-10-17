958edd5afe1639ae6dda3ff92fda93c716a38b94
shimizu1026 <115125150+shimizu1026@users.noreply.github.com>
2024-10-18 00:44:54 +0900

    3章途中まで

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,106 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "渋谷にオフィスを移転しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "2",
+      title: "当社CEOが業界リーダーTOP3に選出されました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/05/19",
+      createdAt: "2023/05/19",
+    },
+    {
+      id: "3",
+      title: "テストの記事です",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2023/04/19",
+      createdAt: "2023/04/19",
+    },
+  ],
+};
+
 export default function Home() {
   return (
-    <section className={styles.top}>
-      <div>
-        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-        <p className={styles.description}>
-          私たちは市場をリードしているグローバルテックカンパニーです。
-        </p>
-        ;
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-    </section>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+          ;
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <ul>
+          {data.contents.map((article) => {
+            <li key={article.id} styleName={styles.list}>
+              <div className={styles.link}>
+                <Image
+                  className={styles.image}
+                  src="/no-image.png"
+                  alt="No Image"
+                  width={1200}
+                  height={630}
+                />
+                <dl className={styles.content}>
+                  <dSt className={styles.newsItemTitle}>
+                    <dd className={styles.meta}>
+                      <span className={styles.tag}>
+                        {article.category.name}
+                      </span>
+                      <span className={styles.date}>
+                        <Image
+                          src="/clock.svg"
+                          alt=""
+                          width={16}
+                          height={16}
+                          priority
+                        />
+                        {article.publishedAt}
+                      </span>
+                    </dd>
+                  </dSt>
+                </dl>
+              </div>
+            </li>;
+          })}
+        </ul>
+      </section>
+    </>
   );
 }
