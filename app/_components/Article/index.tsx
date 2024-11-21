9b07444fe53f1eb7e9f82bd78023580c03013474
shimizu <nichibishimizu@gmail.com>
2024-11-21 09:40:50 +0900

    181

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import type { News } from "@/app/_libs/microcms";
 import Date from "../Date";
@@ -14,8 +15,13 @@ export default function Article({ data }: Props) {
         <h1 className={styles.title}>{data.title}</h1>
         <p className={styles.description}>{data.description}</p>
         <div className={styles.meta}>
+            <Link 
+            href={`/news/category/${data.category.id}`}
+            className={styles.categoryLink}
+            >
             <Category category={data.category}/>
-            <Date date={data.publishedAt ?? data.createdAt}/>
+            </Link>
+            <Date date={data.publishedAt || data.createdAt}/>
         </div>
         {data.thumbnail && (
         <Image 
---
+++ app/_components/news/[slug]/category/id/page.tsx
@@ -0,0 +1,16 @@
+import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function Page({params}: Props) {
+    const { contents: news } = await getNewsList({
+        filters: `category[equals]${params.id}`,
+    })
+
+    return <NewsList news={news}/>;
+}
