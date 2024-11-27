4d50194b74b3eecc2d134bc33ec46e160858db16
shimizu <nichibishimizu@gmail.com>
2024-11-27 15:54:39 +0900

    191

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
 import Category from "@/app/_components/Category";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 type Props = {
   params: {
@@ -12,6 +13,7 @@ type Props = {
 export default async function Page({ params }: Props) {
   const category = await getCategoryDetail(params.id).catch(notFound);
   const { contents: news } = await getNewsList({
+	limit: NEWS_LIST_LIMIT,
     filters: `category[equals]${category.id}`,
   });
 
