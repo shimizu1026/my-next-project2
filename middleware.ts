102fecfbad8c1d72fa5e1f82e0c0c528d34cb224
shimizu <nichibishimizu@gmail.com>
2024-12-11 18:49:35 +0900

    241

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+	console.log("middleware: " + request.url);
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/about/:path*", "/dashboard/:path*"],
+};
